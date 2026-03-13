3b269f4c8abb04709e304a1bad720272d39707ff
Helge Hofstad <helge.hofstad@gmail.com>
2026-03-13 18:38:40 +0100

    fix: inline the logo for ios + add htaccess file

--- src/components/BigLogo.vue
+++ src/components/BigLogo.vue
@@ -23,7 +23,7 @@
 </script>
 
 <template>
-  <v-container :class="containerClass" class="my-5" max-width="900px">
+  <v-container :class="containerClass" class="mt-5 mb-2" max-width="900px">
     <v-slide-y-transition>
       <BigLogoSvg 
         v-show="isLoaded"
--- src/components/VideoOverlay.vue
+++ src/components/VideoOverlay.vue
@@ -1,10 +1,11 @@
 <template>
 	
-  <v-fade-transition>
-    <v-container class="d-flex justify-center videocontainer mb-5" v-show="isLoaded">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/lE8pq3imEiM?si=0CICKHHQm55Wegd_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    </v-container>
-  </v-fade-transition>
+  <v-container class="d-flex justify-center videocontainer mb-5">
+    <v-fade-transition>
+      <iframe v-if="isLoaded" src="https://www.youtube.com/embed/lE8pq3imEiM?si=0CICKHHQm55Wegd_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <div v-if="!isLoaded">&nbsp;</div>
+    </v-fade-transition>
+  </v-container>
 
 </template>
 
@@ -12,6 +13,9 @@
   import { ref, watch } from 'vue'
   const isLoaded = ref(false)
   const props = defineProps({loaded: Boolean})
+  window.toggleMe = function () {
+    isLoaded.value = !isLoaded.value
+  }
   watch(
     () => props.loaded,
     val => {
@@ -23,5 +27,13 @@
 <style scoped>
   .videocontainer {
     overflow: hidden;
+    aspect-ratio: 2.1/1;
+    width: 100%;
+    max-width: 1080px;
+  }
+  iframe {
+    background: rgba(0,0,0,0.3);
+    aspect-ratio: 16/9;
+    width: 100%;
   }
 </style>
